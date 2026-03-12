39f6cf599812d9a106dd182fd4a11b012c035dd0
Facundo Kzemin Paez <facupaezk@gmail.com>
2026-03-11 23:38:33 -0300

    Initial Commit

---
+++ radAR/radARApp.swift
@@ -0,0 +1,17 @@
+//
+//  radARApp.swift
+//  radAR
+//
+//  Created by Facundo Kzemin on 11/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct radARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
